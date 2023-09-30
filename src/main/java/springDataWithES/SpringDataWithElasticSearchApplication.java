7af327b545ba290a608ceafee268517d020f9b1e
bmg13 <gabcmonteiro@gmail.com>
2023-09-30 20:03:05 +0100

    Created service template.

---
+++ src/main/java/springDataWithES/SpringDataWithElasticSearchApplication.java
@@ -0,0 +1,11 @@
+package springDataWithES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataWithElasticSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataWithElasticSearchApplication.class, args);
+    }
+}
